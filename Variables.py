9f856eda2ffefed62e9b25568066d5d3a11dec00
muhammadzain001 <112485536+muhammadzain001@users.noreply.github.com>
2022-08-31 20:01:20 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,35 @@
+
+# Math Operators
+print("Math Operators")
+
+addition = 4+5
+print("4+5 =", addition)
+
+subtraction = 9-8
+print("9-8 =", subtraction)
+
+division = 4/2
+print("4/2 =", division)
+
+multiplication = 13.5 * 13.5
+print("13.5*13.5 = ", multiplication)
+
+
+
+# Exponentiation , Floor Division , and Modulo
+print("Exponentiation , Floor Division , Modulo")
+
+exponentiation = 4 ** 4
+print(exponentiation)
+
+floor_division = (16//5)
+print(floor_division)
+
+Modulo = 7 % 3
+print(Modulo)
+
+Modulo2 = 11 % 3
+print(Modulo2)
+
+
+print("4+5 =", addition)
